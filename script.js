7bb53eea45c781459baa7b18812fe5837a87b3f4
Justin <justinkollie890@yahoo.com>
2021-05-05 10:46:47 -0400

    autoupdate 2021-05-05-10:46:47

--- script.js
+++ script.js
@@ -28,9 +28,13 @@ function appendNumber(number) {
 
 
 // Click the 'AC' button to clear all the display;
-clearButton.addEventListener('click', () => {
+clearButton.addEventListener('click', allClear);
+
+// All Clear Function
+function allClear() {
     showResult.textContent = '0';
-})
+    window.location.reload();
+}
 
 // Click the Delete Button to Delete the last number
 deleteButton.addEventListener('click', deleteLastNumber);
@@ -48,13 +52,10 @@ decmialButton.addEventListener('click', (e) => {
 
 // Append Decimal Numbers
 function appendDecimalNumbers(number) {
-    // if (shouldReset) resetScreen();
     if (showResult.textContent === "") showResult.textContent = "0";
     if (showResult.textContent.includes(".")) return;
     showResult.textContent += number;
 }
-
-
 
 // When operator buttons are clicked, run the setOperation function
 operationSigns.forEach(sign => {
@@ -70,17 +71,18 @@ function setOperation(operator) {
     firstOperand = showResult.textContent;
     currentOperation = operator;
     shouldReset = true;
-    console.log(currentOperation);
 }
 
 // Evaluate Function
 function evaluate() {
     if (currentOperation === '0' || shouldReset) return;
     if (currentOperation === '÷' && showResult.textContent === '0') {
-        // alert('Cannot divide by zero');
         showResult.textContent = 'Cannot divide by zero';
         showResult.style.color = 'red';
-        // clearField() 
+        deleteButton.disabled = true;
+        numberButtons.disabled = true;
+        equalSign.disabled = true;
+        operationSigns.disabled = true;
         return;
     }
     secondOperand = showResult.textContent;
@@ -95,15 +97,13 @@ function roundResult(number) {
 
 equalSign.addEventListener('click', () => {
     evaluate();
-})
+});
 
 // Functions for Bacsic Math Operators
     // add
     function add(a, b){
         let output = a + b;
         output = Number(output);
-        // output = parseFloat(output).toFixed(2);
-        console.log(a + b);
         return showResult.textContent = output;
     }
 
@@ -111,8 +111,6 @@ equalSign.addEventListener('click', () => {
     function subtract(a, b){
         let output = a - b;
         output = Number(output);
-        // output = parseFloat(output);
-        console.log(a - b);
         return showResult.textContent = output;
     }
 
@@ -120,8 +118,6 @@ equalSign.addEventListener('click', () => {
     function multiply(a, b){
         let output = a * b;
         output = Number(output);
-        // output = parseFloat(output);
-        console.log(a * b);
         return showResult.textContent = output;
     }
 
@@ -129,9 +125,7 @@ equalSign.addEventListener('click', () => {
     function divide(a, b){
         let output = a / b;
         output = Number(output);
-        // output = parseFloat(output);
         if (b === 0) return null;
-        console.log(a / b);
         return showResult.textContent = output;
     }
 
